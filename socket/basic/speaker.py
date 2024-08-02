a27b9780cd2c3d5bc50cb27244d09179944cd12b
阿囧 <fpn12099xd@gmail.com>
2024-08-02 22:49:02 +0800

    Create speaker.py

---
+++ socket/basic/speaker.py
@@ -0,0 +1,9 @@
+import socket
+HOST, PORT = '192.168.0.198' , 54321 
+# HOST, PORT = '192.168.178.44' , 54321 
+
+server_addr = (HOST, PORT)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto("Book".encode(), server_addr)
+print('a', end = '') 
